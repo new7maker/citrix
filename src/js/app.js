90931e1c656cb0f793fed258798fffc2f07de117
vskasyrskiy <v.skasurskii@gmail.com>
2022-06-20 13:57:48 +0300

    contacts

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
     flsFunctions.isWebp();
 
 })
+
+function init() {
+    let map = new ymaps.Map('map', {
+        center: [45.05661613902117,42.01743746536805],
+        zoom: 16
+    });
+}
+
+ymaps.ready(init);
